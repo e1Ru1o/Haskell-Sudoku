25ded2e9e8603465d1f01349b21461be3bc1d487
Lazaro Raul <riglesiasvera@gmail.com>
2020-05-09 03:44:39 -0400

    Add the game pipeline

--- Game.hs
+++ Game.hs
@@ -1,10 +1,10 @@
 module Game
-(
+( pipeline
 ) where
 
 import Utils
 import SudokuTypes
-import Data.List (nub, (\\))
+import Data.List (nub, (\\), sort, concat)
 
 -- Cells affinity
 affinity :: Cell -> Cell -> Bool
@@ -20,8 +20,23 @@ affinity (Cell a b c) (Cell d e f)
 related :: Table -> Cell -> [Int]
 related table target
     | length cells > 0 = [1..9] \\ (nub $ map value cells)
-    | otherwise        = []
+    | otherwise        = [1..9]
     where
         cells = filter (affinity target) table
 
+-- Pipeline
+pipeline :: Sudoku -> Sudoku
+pipeline sudoku
+    | done sudoku   = sudoku
+    | otherwise     = pipeline $ fillACell sudoku
 
+fillACell :: Sudoku -> Sudoku
+fillACell sudoku =
+    let Sudoku a b = sudoku in
+    (\(_, v, cell) -> moveCell sudoku cell v) $ head $ sort $ 
+    map (\(v:r, cell) -> ((length r) + 1, v, cell)) [(related b cell, cell) | cell <- a]
+
+moveCell :: Sudoku -> Cell -> Int -> Sudoku
+moveCell s cell v = 
+    let Sudoku a b = s 
+    in  Sudoku (a \\ [cell]) (changeValue cell v : b)
