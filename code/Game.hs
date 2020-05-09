a023f78438928b89f64ba3412ea5c9d1fbff22c0
Lazaro Raul <riglesiasvera@gmail.com>
2020-05-09 16:47:25 -0400

    Implement a backtrack behavior for multi-options cells

--- code/Game.hs
+++ code/Game.hs
@@ -31,11 +31,38 @@ pipeline sudoku
     | done sudoku   = sudoku
     | otherwise     = pipeline $ fillACell sudoku
 
+badSudoku = Sudoku [] [Cell 0 0 0]
+
 fillACell :: Sudoku -> Sudoku
-fillACell sudoku =
-    let Sudoku a b = sudoku in
-    (\(_, v, cell) -> moveCell sudoku cell v) $ head $ sort $ 
-    map (\(v:r, cell) -> ((length r) + 1, v, cell)) [(related b cell, cell) | cell <- a]
+fillACell sudoku 
+    | otherwise     = backtrack sudoku op cell
+    where 
+        Sudoku ceros nums = sudoku
+        foo = map (\(a, b) -> (length a, a, b))
+        relations = [(related nums cell, cell) | cell <- ceros]
+        (cant, op, cell) = head $ sort $ foo relations
+
+backtrack :: Sudoku -> [Int] -> Cell -> Sudoku
+backtrack original l cell
+    | l == []   = badSudoku 
+    | correct   = sudoku
+    | otherwise = backtrack original (tail l) cell
+    where
+        sudoku = pipeline $ moveCell original cell (head l)
+        correct = (sudoku /= badSudoku)
+
+
+-- fillACell :: Sudoku -> Sudoku
+-- fillACell sudoku =
+--     let Sudoku a b = sudoku in
+--     (\(_, v, cell) -> moveCell sudoku cell v) $ head $ sort $ 
+--     map (temp b) [(related b cell, cell) | cell <- a]
+
+temp :: Table -> ([Int], Cell) -> (Int, Int, Cell)
+temp s (l, c)
+    | length l > 0 = let v:_ = l in ((length l) + 1, v, c)
+    | otherwise    = let x = (error "\n" ++ show s ++ "\n" ++ show c ++ "\n") in (0, 0, c)
+
 
 moveCell :: Sudoku -> Cell -> Int -> Sudoku
 moveCell s cell v = 
